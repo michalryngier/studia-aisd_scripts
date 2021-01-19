2abfb1549a597be22cdd7a4169a0423b1b4d3514
m.ryngier <michal.ryngier@gmail.com>
2021-01-19 19:36:04 +0100

    feat: lists

---
+++ TwoWayList.php
@@ -0,0 +1,16 @@
+<?php
+
+
+class TwoWayList
+{
+	public int $index = 0;
+
+	private int $data;
+	private ?TwoWayList $next = null;
+	private ?TwoWayList $prev = null;
+
+	public function __construct(int $data)
+	{
+		$this->data = $data;
+	}
+}
